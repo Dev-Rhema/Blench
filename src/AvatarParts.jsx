480628d00afdc3f9e686473dc221d3f5cf8f874c
Rhema <reymarmcoc@gmail.com>
2025-10-01 10:31:58 +0100

    cloudinary

--- src/AvatarParts.jsx
+++ src/AvatarParts.jsx
@@ -4,12 +4,17 @@ const avatarParts = {
       {
         name: "head",
         colors: {
-          light: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309920/your-folder-name/xkne3z8fdgjidf9uatb9.svg",
-          brown: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309918/your-folder-name/b8i60vvb37h14qkqgy13.svg",
-          black: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309917/your-folder-name/u6elzkusuoyr7oktsmfb.svg",
+          light:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309920/your-folder-name/xkne3z8fdgjidf9uatb9.svg",
+          brown:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309918/your-folder-name/b8i60vvb37h14qkqgy13.svg",
+          black:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309917/your-folder-name/u6elzkusuoyr7oktsmfb.svg",
           blue: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309920/your-folder-name/qfnc3qs35fpypdx0odmg.svg",
-          green: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309923/your-folder-name/pnt5pi4js0ttoke31m7r.svg",
-          white: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309920/your-folder-name/bwm65erjzrxjvwfxxopc.svg",
+          green:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309923/your-folder-name/pnt5pi4js0ttoke31m7r.svg",
+          white:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309920/your-folder-name/bwm65erjzrxjvwfxxopc.svg",
         },
       },
     ],
@@ -19,10 +24,13 @@ const avatarParts = {
       {
         name: "eyes",
         colors: {
-          black: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309918/your-folder-name/tku94okjfcxn4x8whe9x.svg",
+          black:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309918/your-folder-name/tku94okjfcxn4x8whe9x.svg",
           blue: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309915/your-folder-name/d103s4m8o80nib9sklkt.svg",
-          brown: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309921/your-folder-name/ak9cuzzo9pfpiczvf5qu.svg",
-          green: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309915/your-folder-name/tibkthnknog5wazknuur.svg",
+          brown:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309921/your-folder-name/ak9cuzzo9pfpiczvf5qu.svg",
+          green:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309915/your-folder-name/tibkthnknog5wazknuur.svg",
           grey: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309916/your-folder-name/abslrj0dqpri1vykx3il.svg",
           red: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309918/your-folder-name/vurtydlequaunwagp3jg.svg",
         },
@@ -34,34 +42,46 @@ const avatarParts = {
       {
         name: "hair1",
         colors: {
-          black: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309925/your-folder-name/ipcsdz5fdbtervkitemh.svg",
+          black:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309925/your-folder-name/ipcsdz5fdbtervkitemh.svg",
           red: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309919/your-folder-name/by9unaf3jcvc2rvnsuvz.svg",
           blue: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309922/your-folder-name/kbkqyxecxdpda0qpizzd.svg",
-          white: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309926/your-folder-name/mblf5ajwjsjrctkqvvym.svg",
-          green: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309928/your-folder-name/emsn5qaby49lrzdbzheb.svg",
-          yellow: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309925/your-folder-name/fvjrvmthjxdyqa3sylnk.svg",
+          white:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309926/your-folder-name/mblf5ajwjsjrctkqvvym.svg",
+          green:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309928/your-folder-name/emsn5qaby49lrzdbzheb.svg",
+          yellow:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309925/your-folder-name/fvjrvmthjxdyqa3sylnk.svg",
         },
       },
       {
         name: "Hair2",
         colors: {
-          black: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309917/your-folder-name/a8ui35ehnpzxcfxgtium.svg",
+          black:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309917/your-folder-name/a8ui35ehnpzxcfxgtium.svg",
           red: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309920/your-folder-name/totllwq8zjw2nyqnsrm8.svg",
           blue: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309920/your-folder-name/fbjgmaxondny9swfp7wi.svg",
-          white: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309924/your-folder-name/txm1lxkvgtygwbpr6xfi.svg",
-          green: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309921/your-folder-name/x2k1qvkz6eb0fcb2usoe.svg",
-          yellow: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309918/your-folder-name/ijqwnl6xi2cup00tq0zi.svg",
+          white:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309924/your-folder-name/txm1lxkvgtygwbpr6xfi.svg",
+          green:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309921/your-folder-name/x2k1qvkz6eb0fcb2usoe.svg",
+          yellow:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309918/your-folder-name/ijqwnl6xi2cup00tq0zi.svg",
         },
       },
       {
         name: "Hair3",
         colors: {
-          black: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309911/your-folder-name/iqguxllxbizbpapy2ild.svg",
+          black:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309911/your-folder-name/iqguxllxbizbpapy2ild.svg",
           red: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309913/your-folder-name/ldvrpztanjgmth2wzlxg.svg",
           blue: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309912/your-folder-name/ijgswxhvvv4o5s1zefox.svg",
-          white: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309912/your-folder-name/q5ja7zeso3tni0pezk4k.svg",
-          green: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309912/your-folder-name/z8bbj1m132ce9gxx2gl2.svg",
-          yellow: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309912/your-folder-name/pqvewzatihxvkjyedpxo.svg",
+          white:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309912/your-folder-name/q5ja7zeso3tni0pezk4k.svg",
+          green:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309912/your-folder-name/z8bbj1m132ce9gxx2gl2.svg",
+          yellow:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309912/your-folder-name/pqvewzatihxvkjyedpxo.svg",
         },
       },
     ],
@@ -71,21 +91,31 @@ const avatarParts = {
       {
         name: "hoodie",
         colors: {
-          black: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309923/your-folder-name/oalhfiwcgr2fg71m5vqx.svg",
-          brown: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309918/your-folder-name/ce6mml1zmuxikxgagsxd.svg",
-          green: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309921/your-folder-name/szlsa5n1xbymvpzrtncm.svg",
-          white: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309923/your-folder-name/li2jg6pse8jkthht9pya.svg",
-          yellow: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309923/your-folder-name/dykzsftn6hwdwozdi7tb.svg",
+          black:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309923/your-folder-name/oalhfiwcgr2fg71m5vqx.svg",
+          brown:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309918/your-folder-name/ce6mml1zmuxikxgagsxd.svg",
+          green:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309921/your-folder-name/szlsa5n1xbymvpzrtncm.svg",
+          white:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309923/your-folder-name/li2jg6pse8jkthht9pya.svg",
+          yellow:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309923/your-folder-name/dykzsftn6hwdwozdi7tb.svg",
         },
       },
       {
         name: "suit",
         colors: {
-          black: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309911/your-folder-name/wczj7rdv4jsmbmtcevux.svg",
-          brown: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309911/your-folder-name/h12urf2ljbfsnujgama1.svg",
-          green: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309911/your-folder-name/ohttzs8idpb2kbluy0na.svg",
-          white: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309911/your-folder-name/rbelyj24d4f7vqltu7xd.svg",
-          yellow: "https://res.cloudinary.com/dwzojroob/image/upload/v1759309911/your-folder-name/qo2gd26m5tslsoyw0siw.svg",
+          black:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309911/your-folder-name/wczj7rdv4jsmbmtcevux.svg",
+          brown:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309911/your-folder-name/h12urf2ljbfsnujgama1.svg",
+          green:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309911/your-folder-name/ohttzs8idpb2kbluy0na.svg",
+          white:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309911/your-folder-name/rbelyj24d4f7vqltu7xd.svg",
+          yellow:
+            "https://res.cloudinary.com/dwzojroob/image/upload/v1759309911/your-folder-name/qo2gd26m5tslsoyw0siw.svg",
         },
       },
     ],
